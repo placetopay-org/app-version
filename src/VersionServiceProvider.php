6e051f6cfaa19d0e0cecd966fcf43850e28438c2
Diego Calle <dnetix@gmail.com>
2020-05-18 16:20:55 -0500

    Add condiitonal singleton for SentryApi

--- src/VersionServiceProvider.php
+++ src/VersionServiceProvider.php
@@ -2,11 +2,13 @@
 
 namespace PlacetoPay\AppVersion;
 
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use PlacetoPay\AppVersion\Console\Commands\CreateDeploy;
 use PlacetoPay\AppVersion\Console\Commands\CreateRelease;
 use PlacetoPay\AppVersion\Console\Commands\CreateVersionFile;
 use PlacetoPay\AppVersion\Http\Controllers\VersionController;
+use PlacetoPay\AppVersion\Sentry\SentryApi;
 
 class VersionServiceProvider extends ServiceProvider
 {
@@ -27,6 +29,14 @@ class VersionServiceProvider extends ServiceProvider
             ]);
         }
 
+        if (config()->get('app-version.sentry.auth_token')) {
+            $this->app->singleton(SentryApi::class, function (Application $app) {
+                return SentryApi::create(
+                    $app['config']->get('app-version.sentry.auth_token'),
+                    $app['config']->get('app-version.sentry.organization')
+                );
+            });
+        }
         $this->publishes([
             __DIR__ . '/../config/app-version.php' => config_path('app-version.php'),
         ]);
