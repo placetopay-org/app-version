fbc126deed31e484ab547a9c701f3e5db347d962
Kevin Vargas <kevin.vargas@evertecinc.com>
2023-09-25 17:48:35 -0500

    refactor: code structure

--- src/Helpers/Changelog.php
+++ src/Helpers/Changelog.php
@@ -21,11 +21,21 @@ class Changelog
     {
         $content = 'Not available right now';
         if (self::exists()) {
-            $content = file_get_contents(self::path());
-            preg_match_all("/##\s\[\d/", $content, $matches, PREG_OFFSET_CAPTURE);
-            if (count($matches) === 1 && count($matches[0]) > 1) {
-                $content = substr($content, 0, $matches[0][1][1]);
-            }
+            $content = self::getLastVersion(file_get_contents(self::path()));
+        }
+
+        return $content;
+    }
+
+    /**
+     * Returns the content up to the beginning of the second H2 of the Markdown
+     * that meets the following regular expression: /##\s\[\d/ .
+     */
+    private static function getLastVersion(string $content): string
+    {
+        preg_match_all("/##\s\[\d/", $content, $matches, PREG_OFFSET_CAPTURE);
+        if (count($matches) === 1 && count($matches[0]) > 1) {
+            $content = substr($content, 0, $matches[0][1][1]);
         }
 
         return $content;
