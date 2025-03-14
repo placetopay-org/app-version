8ead85996d02bd97c8e013b10f59959401e4b8b6
Ana Maria Granada <ana.granada1@udea.edu.co>
2025-03-14 15:59:09 -0500

    refactor: general changes

--- src/Console/Commands/CreateDeploy.php
+++ src/Console/Commands/CreateDeploy.php
@@ -14,6 +14,7 @@ class CreateDeploy extends Command
     private const GENERAL = 'General';
     private const NEWRELIC = 'Newrelic';
     private const SENTRY = 'Sentry';
+
     /**
      * The name and signature of the console command.
      *
@@ -28,10 +29,6 @@ class CreateDeploy extends Command
      */
     protected $description = 'Creates a new deploy on the available sources';
 
-    /**
-     * @param Repository $config
-     * @return int
-     */
     public function handle(Repository $config): int
     {
 
@@ -84,8 +81,10 @@ class CreateDeploy extends Command
     private function isValidNewRelicConfigurationData(Repository $config): bool
     {
         $validator = Validator::make([
-            'newrelic.api_key' => $config->get('app-version.newrelic.api_key'),
-            'newrelic.entity_guid' => $config->get('app-version.newrelic.entity_guid'),
+            'newrelic' => [
+                'api_key' => $config->get('app-version.newrelic.api_key'),
+                'entity_guid' => $config->get('app-version.newrelic.entity_guid'),
+            ]
         ], [
             'newrelic.api_key' => 'required|string',
             'newrelic.entity_guid' => 'required|string',
@@ -98,8 +97,9 @@ class CreateDeploy extends Command
     private function isValidSentryConfigurationData(Repository $config): bool
     {
         $validator = Validator::make([
-            'sentry.auth_token' => $config->get('app-version.sentry.auth_token'),
-            'sentry.organization' => $config->get('app-version.sentry.organization'),
+            'sentry' => [
+                'auth_token' => $config->get('app-version.sentry.auth_token'),
+                'organization' => $config->get('app-version.sentry.organization'),]
         ], [
             'sentry.auth_token' => 'required|string',
             'sentry.organization' => 'required|string',
@@ -111,7 +111,8 @@ class CreateDeploy extends Command
     private function isValidGeneralData(Repository $config): bool
     {
         $validator = Validator::make([
-            'version.sha' => $config->get('app-version.version.sha'),
+            'version' => [
+                'sha' => $config->get('app-version.version.sha')]
         ], [
             'version.sha' => 'required|string',
         ]);
@@ -124,7 +125,7 @@ class CreateDeploy extends Command
         try {
             $validator->validate();
         } catch (ValidationException $e) {
-            $this->error("$deployType configuration is not valid: \n" . implode("\n", $validator->errors()->all()));
+            $this->warn("$deployType configuration is not valid:\n" . implode("\n", $validator->errors()->all()));
             return false;
         }
 
--- tests/Commands/CreateDeployCommandTest.php
+++ tests/Commands/CreateDeployCommandTest.php
@@ -54,4 +54,41 @@ class CreateDeployCommandTest extends TestCase
 
         $this->assertEquals($this->fakeClient->lastRequest()['headers'][0], 'API-Key: ' . config('app-version.newrelic.api_key'));
     }
+
+    /** @test */
+    public function can_not_create_a_release_if_has_invalid_version_data()
+    {
+
+        config()->set('app-version.version.sha', '');
+
+        $this
+            ->artisan('app-version:create-deploy')
+            ->expectsOutput(
+                "General configuration is not valid:\nThe version.sha field is required."
+            );
+    }
+
+    /** @test */
+    public function can_not_create_a_release_if_has_invalid_data()
+    {
+        config()->set('app-version.version.sha', 'asdfg2');
+
+        config()->set('app-version.newrelic', [
+            'api_key' => '',
+            'entity_guid' => '',
+        ]);
+
+        config()->set('app-version.sentry', [
+            'auth_token' => '',
+            'organization' => '',
+        ]);
+
+        $this
+            ->artisan('app-version:create-deploy')
+            ->assertSuccessful()
+            ->expectsOutput("Sentry configuration is not valid:\nThe sentry.auth token field is required.\nThe sentry.organization field is required.")
+            ->expectsOutput("Newrelic configuration is not valid:\nThe newrelic.api key field is required.\nThe newrelic.entity guid field is required.")
+            ->doesntExpectOutput("General configuration is not valid:\nThe version.sha field is required.");
+    }
+
 }
