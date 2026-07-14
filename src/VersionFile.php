27a05858eaf9eab759703cc3a6f04c8b3583f372
Alejandro Alvarez <alejandro.alvarez@evertecinc.com>
2026-07-14 16:23:16 -0500

    fix: update json_decode to return associative array

--- src/VersionFile.php
+++ src/VersionFile.php
@@ -29,7 +29,7 @@ class VersionFile
     public static function read(): array
     {
         if (self::exists()) {
-            return json_decode(file_get_contents(self::path()), JSON_OBJECT_AS_ARRAY);
+            return json_decode(file_get_contents(self::path()), true);
         }
 
         return [];
